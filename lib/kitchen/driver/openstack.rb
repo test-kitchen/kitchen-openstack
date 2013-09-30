d968573bcb90ccd92d1ace9f1c9fb53b6a8d3d40
Walter  Huf <walter.huf@corvisa.com>
2013-09-30 08:34:53 -0500

    Removes extraneous variable assignment

--- lib/kitchen/driver/openstack.rb
+++ lib/kitchen/driver/openstack.rb
@@ -108,13 +108,11 @@ module Kitchen
         pool_name = config[:floating_ip_pool]
         ips = compute.addresses.all
         free_addresses = ips.delete_if {|p| p.pool!=pool_name || p.instance_id}
-        created = false
         ip = nil
         while free_addresses.count > 0 and ip.nil?
           # try to claim an ip
           begin
             server.associate_address(free_addresses[0].ip)
-            created = false
             ip = free_addresses[0]
             puts "Reusing existing ip #{ip.ip}"
           rescue
@@ -123,7 +121,6 @@ module Kitchen
         end
         if ip.nil?
           # allocate a new ip
-          created = true
           ip = compute.addresses.create({:pool=>pool_name,
                                          :server=>server})
           puts "Allocated new ip #{ip.ip}"
