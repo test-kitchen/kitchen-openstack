94c0e5d20c561ac8576f31e4b2de632715db2010
Jonathan Hartman <j@p4nt5.com>
2015-01-07 22:18:04 -0800

    Bump version for release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# ?.?.? / ????-??-??
+# 1.7.1 / 2015-01-07
 
 * PR [#70][] - Use configured password for SSH access, if provided
 
--- lib/kitchen/driver/openstack_version.rb
+++ lib/kitchen/driver/openstack_version.rb
@@ -21,6 +21,6 @@ module Kitchen
   #
   # @author Jonathan Hartman <j@p4nt5.com>
   module Driver
-    OPENSTACK_VERSION = '1.7.1.dev'
+    OPENSTACK_VERSION = '1.7.1'
   end
 end
