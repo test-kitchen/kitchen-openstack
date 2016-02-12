1c9d0bce4f7e4ff33ebdeb35e45beebd7068b500
JJ Asghar <jj@chef.io>
2016-02-12 11:53:18 -0600

    3.0.0 Release of kitchen-openstack
    - Updated README
    - Removed the `:username` and `:password` options
    - Removed the `:winrm_wait` because it wasn't being used
    - Removed the `setup_ssh` because it was doubling up what transport was doing

--- lib/kitchen/driver/openstack_version.rb
+++ lib/kitchen/driver/openstack_version.rb
@@ -3,7 +3,7 @@
 # Author:: Jonathan Hartman (<j@p4nt5.com>)
 #
 # Copyright (C) 2013-2015, Jonathan Hartman
-# Copyright (C) 2015, Chef Software Inc
+# Copyright (C) 2015-2016, Chef Software Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,6 +22,6 @@ module Kitchen
   #
   # @author Jonathan Hartman <j@p4nt5.com>
   module Driver
-    OPENSTACK_VERSION = '2.2.0'
+    OPENSTACK_VERSION = '3.0.0.pre.1'
   end
 end
